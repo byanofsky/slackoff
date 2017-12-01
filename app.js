88fa70f579cca08ec17cb8d340a32b26dd1de441
byanofsky <byanofsky@me.com>
2017-12-01 14:47:20 -0800

    Move db url to .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const httpPre = require('http');
 const socketIO = require('socket.io');
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/slackoff');
+mongoose.connect(process.env.DB_URL);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
