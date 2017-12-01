65bc6894a9542fa87f669723027886e1a746359a
byanofsky <byanofsky@me.com>
2017-12-01 13:53:46 -0800

    Log socket user disconnect

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const httpPre = require('http');
-const socket = require('socket.io');
+const socketIO = require('socket.io');
 const messageController = require('./controllers/messageController');
 
 const app = express();
 const http = httpPre.Server(app);
-const io = socket(http);
+const io = socketIO(http);
 
 app.use(express.static('public'));
 app.use(express.json());
@@ -14,8 +14,11 @@ app.get('/messages', messageController.getMessages);
 
 app.post('/messages', messageController.createMessage);
 
-io.on('connection', function(socket){
+io.on('connection', (socket) => {
   console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, () => console.log('Example app listening on port 3000!'));
