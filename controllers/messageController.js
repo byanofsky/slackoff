b56fab391750a011a44f963299bd83316a0cc8db
byanofsky <byanofsky@me.com>
2017-12-01 11:59:11 -0800

    Handle errors with 500

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -5,7 +5,7 @@ const messageController = {};
 messageController.createMessage = (req, res) => {
   const messageInst = { user: req.body.user, message: req.body.message };
   Message.create(messageInst, (err, msg) => {
-    if (err) throw err;
+    if (err) return res.status(500).json(err);
     return res.json(msg);
   });
 };
