9a8a342781595b616cb63efe8fd0d4240caa9a6d
byanofsky <byanofsky@me.com>
2017-12-02 12:53:59 -0800

    Handle scrolling

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-import io from 'socket.io-client';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -12,46 +10,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-// $(() => {
-//
-//   function scrollMessages() {
-//     $messagesWrapperNode.animate({ scrollTop: $messagesWrapperNode.prop('scrollHeight') });
-//   }
-//   function formatMessage(msg) {
-//     let sentiment = 0;
-//     if (msg.sentScore < -2) {
-//       sentiment = '-2';
-//     } else if (msg.sentScore < -1) {
-//       sentiment = '-1';
-//     } else if (msg.sentScore < 1) {
-//       sentiment = '0';
-//     } else if (msg.sentScore < 2) {
-//       sentiment = '1';
-//     } else if (msg.sentScore >= 2) {
-//       sentiment = '2';
-//     }
-//     return `
-//       <li data-sent="${sentiment}" data-me="${msg.user === user}">
-//         <div class="message">
-//           ${msg.message}
-//           <footer>${msg.user} at ${new Date(msg.created).toLocaleTimeString()}</footer>
-//         </div>
-//       </li>
-//     `;
-//   }
-//
-//   socket.on('new message', (msgObjStr) => {
-//     const msgObj = JSON.parse(msgObjStr);
-//     $messagesNode.append($(formatMessage(msgObj)));
-//     scrollMessages();
-//   });
-//   socket.on('all messages', (messagesJSON) => {
-//     const messages = JSON.parse(messagesJSON);
-//     $messagesNode.html(messages.map(formatMessage).join(''));
-//     scrollMessages();
-//   });
-//   socket.on('exception', error => console.error(error));
-//
-//   $messageFormNode.submit(handleSubmit);
-// });
